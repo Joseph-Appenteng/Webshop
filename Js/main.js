db02d76380476d277ad716b3756f42e46ec0349b
Joseph <josephappenteng99@gmail.com>
2023-06-24 20:30:28 +0200

    Homepagina.html

--- Js/main.js
+++ Js/main.js
@@ -156,7 +156,9 @@ const addToCartButtons = document.getElementsByClassName("games__button");
 // Variabele om het aantal items in de winkelwagen bij te houden
 let cartMessage = 0;
 // Selecteer het element waar het winkelwagenbericht wordt weergegeven
-const cartMessageValue = document.getElementsByClassName("shoppingCart__message")[0];
+const cartMessageValue = document.getElementsByClassName(
+  "shoppingCart__message"
+)[0];
 // Selecteer het modaal venster element
 const shoppinModal = document.getElementById("js--shoppinModal");
 // Variabele om bij te houden of het modaal venster open is of niet
@@ -176,7 +178,7 @@ for (let i = 0; i < addToCartButtons.length; i++) {
     cartMessage += 1;
     // Update de weergave van het winkelwagenbericht op de pagina
     cartMessageValue.innerHTML = cartMessage;
-    
+
     // Bepaal welk product is toegevoegd op basis van de dataset van de geklikte knop
     switch (addToCartButtons[i].dataset.product) {
       case "elden":
@@ -210,9 +212,31 @@ for (let i = 0; i < addToCartButtons.length; i++) {
     }
   };
 }
+// Selecteer het linkerpijlsymbool
+const leftArrow = document.querySelector(".fa-arrow-left");
 
-// Verberg het linkerpijlsymbool
-document.getElementsByClassName("fa-arrow-left")[0].style.display = "none";
+// Klikgebeurtenis voor het linkerpijlsymbool
+leftArrow.onclick = function () {
+  // Verberg het afrekenvenster
+  checkoutWindow.style.display = "none";
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  };
+
+  // Reset de variabele om bij te houden of het afrekenvenster open is of niet
+  checkoutIsOpen = false;
+
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  };
+
+  // Toon de hoofdpagina
+  document.querySelector("main").style.display = "block";
+  document.querySelector(".container").style.display = "block";
+  document.querySelector(".navbar").style.display = "flex";
+  document.querySelector(".header").style.display = "flex";
+  document.getElementsByClassName("searchBar__glass")[0].style.display = "flex";
+};
 
 // Selecteer de "checkout" knop
 const checkoutButton = document.getElementById("js--checkoutButton");
@@ -226,37 +250,68 @@ checkoutButton.onclick = function () {
   // Als het afrekenvenster nog niet geopend is, open het dan
   if (checkoutIsOpen === false) {
     document.querySelector("main").style.display = "none";
-    document.getElementsByClassName("fa-arrow-left")[0].style.display = "block";
     document.querySelector(".container").style.display = "none";
+    document.querySelector(".navbar").style.display = "none";
+    document.querySelector(".header").style.display = "none";
     checkoutWindow.style.display = "block";
     checkoutIsOpen = true;
-    document.getElementsByClassName("fa-arrow-left")[0].style.display = "block";
     document.getElementById("js--amount-elden").innerHTML = elden + "x";
-    document.getElementById("js--prijs-elden").innerHTML = "€" + elden * 52;
+    document.getElementById("js--prijs-elden").innerHTML = "€" + elden * 42;
     document.getElementById("js--amount-hogwarts").innerHTML = hogwarts + "x";
     document.getElementById("js--amount-resident").innerHTML = resident + "x";
     document.getElementById("js--amount-star").innerHTML = star + "x";
     document.getElementById("js--amount-street").innerHTML = street + "x";
     document.getElementById("js--amount-mortal").innerHTML = mortal + "x";
-    document.getElementById("js--prijs-hogwarts").innerHTML = "€" + hogwarts * 41;
-    document.getElementById("js--prijs-resident").innerHTML = "€" + resident * 40;
+    document.getElementById("js--prijs-hogwarts").innerHTML =
+      "€" + hogwarts * 41;
+    document.getElementById("js--prijs-resident").innerHTML =
+      "€" + resident * 40;
     document.getElementById("js--prijs-star").innerHTML = "€" + star * 47;
     document.getElementById("js--prijs-street").innerHTML = "€" + street * 50;
     document.getElementById("js--prijs-mortal").innerHTML = "€" + mortal * 57;
-    document.getElementsByClassName("fa-cart-shopping")[0].style.display =
-      "none";
     document.getElementsByClassName("searchBar__glass")[0].style.display =
       "none";
     return;
   }
   // Als het afrekenvenster al geopend is, sluit het dan
-  document.getElementsByClassName("fa-arrow-left")[0].style.display = "none";
-  document.querySelector("main").style.display = "block";
+  document.querySelector("main").style.display = "none";
+  document.querySelector(".container").style.display = "none";
   checkoutWindow.style.display = "none";
   checkoutIsOpen = false;
   searchBar.style.display = "block";
-  document.getElementsByClassName("fa-cart-shopping")[0].style.display =
-    "block";
+  document.getElementsByClassName("fa-cart-shopping")[0].style.display = "none";
   document.getElementsByClassName("searchBar__glass")[0].style.display = "flex";
 };
 
+//prijs filter//
+function changeProduct() {
+  let select = document.getElementById("prijs");
+  let selectedOption = select.options[select.selectedIndex].text;
+
+  let games = document.getElementsByClassName("games__game");
+
+  // Loop through all the games and show/hide based on the selected price range
+  for (let i = 0; i < games.length; i++) {
+    let game = games[i];
+    let priceElement = game.querySelector(".games__h2");
+
+    if (priceElement) {
+      let price = parseFloat(priceElement.innerText.replace("€", ""));
+
+      // Show the game if it falls within the selected price range
+      if (selectedOption === "Prijs" || isPriceInRange(selectedOption, price)) {
+        game.style.display = "block";
+      } else {
+        game.style.display = "none";
+      }
+    }
+  }
+}
+
+function isPriceInRange(range, price) {
+  let rangeValues = range.split("-");
+  let minPrice = parseFloat(rangeValues[0].replace("€", ""));
+  let maxPrice = parseFloat(rangeValues[1].replace("€", ""));
+
+  return price >= minPrice && price <= maxPrice;
+}
--- Js/producten.js
+++ Js/producten.js
@@ -67,6 +67,135 @@ actionFilter.onchange = function () {
   }
 };
 
+// Selecteer alle knoppen met de class "games__button"
+const addToCartButtons = document.getElementsByClassName("games__button");
+// Variabele om het aantal items in de winkelwagen bij te houden
+let cartMessage = 0;
+// Selecteer het element waar het winkelwagenbericht wordt weergegeven
+const cartMessageValue = document.getElementsByClassName(
+  "shoppingCart__message"
+)[0];
+// Selecteer het modaal venster element
+const shoppinModal = document.getElementById("js--shoppinModal");
+// Variabele om bij te houden of het modaal venster open is of niet
+let modalIsOpen = false;
+// Variabelen om bij te houden hoeveel items van elke soort in de winkelwagen zijn
+let elden = 0;
+let hogwarts = 0;
+let resident = 0;
+let star = 0;
+let street = 0;
+let mortal = 0;
+
+// Voeg klikgebeurtenissen toe aan de "addToCart" knoppen
+for (let i = 0; i < addToCartButtons.length; i++) {
+  addToCartButtons[i].onclick = function () {
+    // Verhoog het aantal items in de winkelwagen
+    cartMessage += 1;
+    // Update de weergave van het winkelwagenbericht op de pagina
+    cartMessageValue.innerHTML = cartMessage;
+
+    // Bepaal welk product is toegevoegd op basis van de dataset van de geklikte knop
+    switch (addToCartButtons[i].dataset.product) {
+      case "elden":
+        elden += 1;
+        break;
+      case "hogwarts":
+        hogwarts += 1;
+        break;
+      case "resident":
+        resident += 1;
+        break;
+      case "star":
+        star += 1;
+        break;
+      case "street":
+        street += 1;
+        break;
+      case "mortal":
+        mortal += 1;
+        break;
+    }
+
+    // Als het modaal venster nog niet geopend is, open het dan en sluit het na 2 seconden
+    if (modalIsOpen === false) {
+      shoppinModal.style.display = "flex";
+      modalIsOpen = true;
+      setTimeout(function () {
+        shoppinModal.style.display = "none";
+        modalIsOpen = false;
+      }, 2000);
+    }
+  };
+}
+// Selecteer het linkerpijlsymbool
+const leftArrow = document.querySelector(".fa-arrow-left");
+
+// Klikgebeurtenis voor het linkerpijlsymbool
+leftArrow.onclick = function () {
+  // Verberg het afrekenvenster
+  checkoutWindow.style.display = "none";
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  };
+
+  // Reset de variabele om bij te houden of het afrekenvenster open is of niet
+  checkoutIsOpen = false;
+
+  // Toon de hoofdpagina
+  document.querySelector("main").style.display = "block";
+  document.querySelector(".container").style.display = "block";
+  document.querySelector(".navbar").style.display = "flex";
+  document.querySelector(".header").style.display = "flex";
+  document.getElementsByClassName("searchBar__glass")[0].style.display = "flex";
+};
+
+// Selecteer de "checkout" knop
+const checkoutButton = document.getElementById("js--checkoutButton");
+// Selecteer het afrekenvenster
+const checkoutWindow = document.getElementById("js--checkoutWindow");
+// Variabele om bij te houden of het afrekenvenster open is of niet
+let checkoutIsOpen = false;
+
+// Klikgebeurtenis voor de "checkout" knop
+checkoutButton.onclick = function () {
+  // Als het afrekenvenster nog niet geopend is, open het dan
+  if (checkoutIsOpen === false) {
+    document.querySelector("main").style.display = "none";
+    document.querySelector(".container").style.display = "none";
+    document.querySelector(".navbar").style.display = "none";
+    document.querySelector(".header").style.display = "none";
+
+    checkoutWindow.style.display = "block";
+    checkoutIsOpen = true;
+    document.getElementById("js--amount-elden").innerHTML = elden + "x";
+    document.getElementById("js--prijs-elden").innerHTML = "€" + elden * 42;
+    document.getElementById("js--amount-hogwarts").innerHTML = hogwarts + "x";
+    document.getElementById("js--amount-resident").innerHTML = resident + "x";
+    document.getElementById("js--amount-star").innerHTML = star + "x";
+    document.getElementById("js--amount-street").innerHTML = street + "x";
+    document.getElementById("js--amount-mortal").innerHTML = mortal + "x";
+    document.getElementById("js--prijs-hogwarts").innerHTML =
+      "€" + hogwarts * 41;
+    document.getElementById("js--prijs-resident").innerHTML =
+      "€" + resident * 40;
+    document.getElementById("js--prijs-star").innerHTML = "€" + star * 47;
+    document.getElementById("js--prijs-street").innerHTML = "€" + street * 50;
+    document.getElementById("js--prijs-mortal").innerHTML = "€" + mortal * 57;
+    document.getElementsByClassName("searchBar__glass")[0].style.display =
+      "none";
+    return;
+  }
+  // Als het afrekenvenster al geopend is, sluit het dan
+  document.querySelector("main").style.display = "none";
+  document.querySelector(".container").style.display = "none";
+  checkoutWindow.style.display = "none";
+  checkoutIsOpen = false;
+  searchBar.style.display = "block";
+  document.getElementsByClassName("fa-cart-shopping")[0].style.display = "none";
+  document.getElementsByClassName("searchBar__glass")[0].style.display = "flex";
+};
+
 //prijs filter//
 function changeProduct() {
   let select = document.getElementById("prijs");
